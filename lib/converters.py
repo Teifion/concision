59dcb80332704c063a860b9ecc04af2a284cb7ff
Teifion <sarkalian@gmail.com>
2013-06-11 23:20:18 +0100

    Moved conversion code to converters
    It just makes sense to me...

--- lib/converters.py
+++ lib/converters.py
@@ -1,3 +1,16 @@
+import datetime
+import re
+from decimal import Decimal
+
+from sqlalchemy.types import (
+    Integer as sql_integer,
+    Numeric as sql_numeric,
+    Float as sql_float,
+    String as sql_string,
+    Date as sql_date,
+    DateTime as sql_datetime,
+)
+
 func_dict = {
     "upper": lambda v: v.upper(),
     "strip": lambda v: v.strip(),
@@ -7,3 +20,49 @@ def convert(value, funcs):
     for f in [func_dict[fname] for fname in funcs]:
         value = f(value)
     return value
+
+template_re = re.compile(r"\{[a-zA-Z0-9_]+\}")
+def apply_template(value):
+    if "{today}" in value:
+        value = value.replace("{today}", datetime.date.today().strftime("%d/%m/%Y"))
+    
+    if "{yesterday}" in value:
+        today = datetime.date.today()
+        value = value.replace("{yesterday}", (today - datetime.timedelta(days=1)).strftime("%d/%m/%Y"))
+    
+    if template_re.search(value):
+        raise ValueError("Template item still remaining in '%s'" % value)
+    
+    return value
+
+def typecast(filter_col, value):
+    """
+    Converts the value from a string into the correct datatype
+    for the database.
+    """
+    
+    ptype = type(filter_col.type)
+    value = apply_template(value)
+    
+    if ptype == sql_date:
+        temp_value = value.replace(" ", "/").replace("-", "/")
+        d, m, y = temp_value.split("/")
+        
+        return datetime.date(year=int(y), month=int(m), day=int(d))
+        
+    elif ptype == sql_datetime:
+        # TODO: Accept times as well as just dates
+        temp_value = value.replace(" ", "/").replace("-", "/")
+        d, m, y = temp_value.split("/")
+        return datetime.datetime(year=int(y), month=int(m), day=int(d))
+    
+    elif ptype == sql_numeric:
+        return Decimal(value)
+    elif ptype == sql_integer:
+        return int(value)
+    elif ptype == sql_float:
+        return float(value)
+    elif ptype == sql_string:
+        return value
+    else:
+        raise KeyError("No handler for %s" % ptype)
--- lib/query_f.py
+++ lib/query_f.py
@@ -1,18 +1,6 @@
-import json
-from decimal import Decimal
 from .. import config
 from sqlalchemy.orm import aliased
-import datetime
-import re
-from . import converters, html_f, consts
-
-from sqlalchemy.types import (
-    Integer as sql_integer,
-    Numeric as sql_numeric,
-    String as sql_string,
-    Date as sql_date,
-    DateTime as sql_datetime,
-)
+from . import converters, consts
 
 class CQuery(object):
     """An object for storing the query being built."""
@@ -29,7 +17,7 @@ class CQuery(object):
     def check_for_aliases(self, data):
         """
         Goes through all referenced fields and checks to see if they
-        are in the alias list. Any it finds it adds aliases for and 
+        are in the alias list. Any it finds it adds aliases for and
         extends the filters so the alias will be linked to.
         """
         total_columns = data['columns'] + [data['key']] + [f['column'] for f in data['filters']]
@@ -83,56 +71,6 @@ class CQuery(object):
         # If not then grab the column itself
         return config['metadata'].tables[table].columns[column]
 
-template_re = re.compile(r"\{[a-zA-Z0-9_]+\}")
-def apply_template(value):
-    if "{today}" in value:
-        value = value.replace("{today}", datetime.date.today().strftime("%d/%m/%Y"))
-    
-    if "{yesterday}" in value:
-        today = datetime.date.today()
-        value = value.replace("{yesterday}", (today - datetime.timedelta(days=1)).strftime("%d/%m/%Y"))
-    
-    if template_re.search(value):
-        raise ValueError("Template item still remaining in '%s'" % value)
-    
-    return value
-
-def typecast(filter_col, value):
-    """
-    Converts the value from a string into the correct datatype
-    for the database.
-    """
-    # dat = []
-    # dat.append(str(dir(filter_col)))
-    # for d in dir(filter_col):
-    #     dat.append("%s: %s" % (d, getattr(filter_col, d)))
-    # raise Exception("\n\n".join(dat))
-    
-    ptype = type(filter_col.type)
-    value = apply_template(value)
-    
-    if ptype == sql_date:
-        try:
-            d, m, y = value.split("/")
-        except Exception as e:
-            raise e
-        
-        return datetime.date(year=int(y), month=int(m), day=int(d))
-        
-    elif ptype == sql_datetime:
-        # TODO: Accept times as well as just dates
-        d, m, y = value.split("/")
-        return datetime.datetime(year=int(y), month=int(m), day=int(d))
-    
-    elif ptype == sql_numeric:
-        return Decimal(value)
-    elif ptype == sql_integer:
-        return int(value)
-    elif ptype == sql_string:
-        return value
-    else:
-        raise KeyError("No handler for %s" % ptype)
-
 def build(data):
     q = CQuery()
     q.check_for_aliases(data)
@@ -154,10 +92,11 @@ def build(data):
         # config['sources'][table]
         # if column in table_source.aliases:
         
-        value = converters.convert(f['value'], source_table.column_converters.get(c, []))
-        # value = convert_value(f['value'], [])
+        # Convert it from a string into the relevant database type
+        value = converters.typecast(q.get(f['column'], use_alias=False), f['value'])
         
-        value = typecast(q.get(f['column'], use_alias=False), value)
+        # Apply conversion functions to it
+        value = converters.convert(value, source_table.column_converters.get(c, []))
         
         q.filters.append(op_func(value))
     
